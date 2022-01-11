7f1c3bfe4910b6a7e71174df314109690f0878cd
Antoine Khoury <antoine.khoury@scaiconsulting.it>
2022-01-11 10:41:35 +0100

    added ionic with calendar

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path: 'calendar',
+    loadChildren: () => import('./calendar/calendar.module').then( m => m.CalendarPageModule)
   }
 ];
 
